e905f50b28359c3b69c0c5f99c34d92d0fe5081e
Jm072700 <80952813+Jm072700@users.noreply.github.com>
2022-12-15 17:24:08 +0800

    test

--- src/components/Phone.js
+++ src/components/Phone.js
@@ -9,7 +9,6 @@ const Phone = ({phones , handleClick , handleInfo}) => {
             <div className='card-body'>
               <div className='text-center'>
               <h5 className='card-title'>{phone.name}</h5>
-              <p className='card-text font-weight-light'>{phone.text}</p>
               <button className='btn btn-light text-center rounded-pill text-black border-dark' >Buy Now</button>
               <button className='link-dark text-center rounded text-black border-0 hover-underline-animation bg-light p-2' onClick={()=>{handleClick();handleInfo(phone.id);}}>Learn more</button>
               </div>
