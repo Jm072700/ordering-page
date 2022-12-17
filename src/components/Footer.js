afa89f5cca31af3efbc4bc3f727b302f8e930f0d
Jm072700 <80952813+Jm072700@users.noreply.github.com>
2022-12-17 10:38:14 +0800

    added icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <>
     <footer className="py-3 my-4">
       <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+        <Link to={'/'} className='link'>
         <li className="nav-item"><h5 className="nav-link px-2 text-muted">Home</h5></li>
+        </Link>
         <li className="nav-item"><h5 className="nav-link px-2 text-muted">Features</h5></li>
         <li className="nav-item"><h5 className="nav-link px-2 text-muted">Pricing</h5></li>
         <li className="nav-item"><h5 className="nav-link px-2 text-muted">FAQs</h5></li>
